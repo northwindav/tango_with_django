2b66a591a95a642e19b39e8fb0251261a932e921
Mike Smith <northwind.avalanche@gmail.com>
2015-01-07 20:06:27 -0800

    finished exercise at end of http://www.tangowithdjango.com/book17/chapters/setup.html

---
+++ rango/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from rango import views
+
+# For django to find the url mappings, this
+# tupple must be called urlpatterns
+urlpatterns =  patterns('',
+	url(r'^$', views.index, name='index'),
+	url(r'^about/', views.about, name='about'))
