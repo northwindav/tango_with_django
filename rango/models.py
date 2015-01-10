c1add3d4bf4ab4703fd3c9514d8bfc6cd16e141a
Mike Smith <northwind.avalanche@gmail.com>
2015-01-10 06:01:12 -0800

    Done with lesson 6

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # Since Unique=TRUE then category name must be unique
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 	class Meta:
 		verbose_name_plural = "Categories"
 	
