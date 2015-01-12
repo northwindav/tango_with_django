c516c3856f288e4504783ec142774f2420f1c979
Mike Smith <northwind.avalanche@gmail.com>
2015-01-12 05:41:46 -0800

    Same as previous commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,6 +66,14 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+# registration
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                # and are trying to access pages requiring authentication
 
 # This is not required if we use the default PBKDF2 algorightm,
 # but adding it here for completeness
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls'))
+    url(r'^rango/', include('rango.urls')),
+    (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 # this combined with the settings module allows us to access
